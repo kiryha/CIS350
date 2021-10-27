12fad4050a49aab9ac2e184412186e7ed719d532
Kiryha <kiryha@gmail.com>
2021-10-27 18:25:07 -0400

    update tree print

--- program_2/program.py
+++ program_2/program.py
@@ -112,35 +112,35 @@ def build_tree_structure(node):
         lines, n, p, x = build_tree_structure(node.left)
         s = '{}'.format(node.data)
         u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-        second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-        shifted_lines = [line + u * ' ' for line in lines]
-        return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+        first_line = (x+1)*' '+(n-x-1)*'_'+s
+        second_line = x*' '+'/'+(n-x-1+u)*' '
+        shifted_lines = [line+u*' ' for line in lines]
+        return [first_line, second_line] + shifted_lines, n+u, p+2, n+u//2
 
     # Only right child
     if node.left is None:
         lines, n, p, x = build_tree_structure(node.right)
         s = '{}'.format(node.data)
         u = len(s)
-        first_line = s + x * '_' + (n - x) * ' '
-        second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-        shifted_lines = [u * ' ' + line for line in lines]
-        return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+        first_line = s+x*'_'+(n-x)*' '
+        second_line = (u+x)*' '+'\\'+(n-x-1)*' '
+        shifted_lines = [u*' '+line for line in lines]
+        return [first_line, second_line] + shifted_lines, n+u, p+2, u//2
 
     # Two children
     left, n, p, x = build_tree_structure(node.left)
     right, m, q, y = build_tree_structure(node.right)
     s = '{}'.format(node.data)
     u = len(s)
-    first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-    second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    first_line = (x+1)*' '+(n-x-1)*'_'+s+y*'_'+(m-y)*' '
+    second_line = x*' '+'/'+(n-x-1+u+y)*' '+'\\'+(m-y-1)*' '
     if p < q:
-        left += [n * ' '] * (q - p)
+        left += [n*' ']*(q-p)
     elif q < p:
-        right += [m * ' '] * (p - q)
+        right += [m*' ']*(p-q)
     zipped_lines = zip(left, right)
-    lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-    return lines, n + m + u, max(p, q) + 2, n + u // 2
+    lines = [first_line, second_line] + [a+u*' '+b for a, b in zipped_lines]
+    return lines, n+m+u, max(p, q)+2, n+u//2
 
 
 class Node:
