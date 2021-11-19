33cbce9830eabc9ebcfbaa954171c04dc627ae7f
Kiryha <kiryha@gmail.com>
2021-11-18 21:18:31 -0500

    add graph implementation

--- program_3/program.py
+++ program_3/program.py
@@ -17,11 +17,54 @@ import os
 program_root = os.path.dirname(os.path.abspath(__file__))
 
 
+class Vertex:
+    """ The adjacency list Vertex object """
+    def __init__(self, vertex_id, weight):
+        self.vertex_id = vertex_id
+        self.weight = weight
+        self.next = None
+
+
+class Graph:
+    """
+    Adjacent List representation of a graph
+    """
+    def __init__(self, number_of_vertices):
+        self.number_of_vertices = number_of_vertices
+        self.graph = [None] * self.number_of_vertices
+        print self.graph
+
+    def add_edge(self, vertex_id_1, vertex_id_2, weight):
+
+        vertex = Vertex(vertex_id_1, weight)
+        vertex.next = self.graph[vertex_id_2]
+        self.graph[vertex_id_2] = vertex
+
+        vertex = Vertex(vertex_id_2, weight)
+        vertex.next = self.graph[vertex_id_1]
+        self.graph[vertex_id_1] = vertex
+
+    def build_graph_string(self):
+
+        graph_string = ''
+
+        for i in range(self.number_of_vertices):
+            graph_string += 'Vertex [{}]: '.format(i)
+            vertex = self.graph[i]
+            while vertex:
+                graph_string += '({0}, {1}) '.format(vertex.vertex_id, vertex.weight)
+                vertex = vertex.next
+            graph_string += " \n"
+
+        return graph_string
+
+
 def read_data(file_path):
     """
     Read input data from file, convert to a dictionary of graphs:
-        {index_of_graph: [ edge, ... ], ...}
-        where edge = [ node_id_1, node_id_2, edge weight]
+        {index_of_graph: {properties: [number_of_nodes, number_of_edges]
+                          edges: [edge_1, ... ]}
+        where edge = [ vertex_id_1, vertex_id_2, edge weight]
 
     :param file_path: path to a file with graph data
     :return: graph dictionary
@@ -49,7 +92,9 @@ def read_data(file_path):
                     index += 1
 
                 # Init new graph
-                graphs_data[index] = []
+                graphs_data[index] = {'properties': {'number_of_vertices': int(line_content[0]),
+                                                     'number_of_edges': int(line_content[1])},
+                                      'edges': []}
 
             # Skip empty line
             elif len(line_content) == 0:
@@ -57,11 +102,23 @@ def read_data(file_path):
 
             # Parse graph data
             else:
-                graphs_data[index].append(line_content)
+                graphs_data[index]['edges'].append([int(line_content[0]), int(line_content[1]), int(line_content[2])])
     
     return graphs_data
 
-                
+
+def process_graph(graph_data):
+
+    number_of_vertices = graph_data['properties']['number_of_vertices']
+    edges = graph_data['edges']
+    graph = Graph(number_of_vertices)
+
+    for edge in edges:
+        graph.add_edge(edge[0], edge[1], edge[2])
+
+    print graph.build_graph_string()
+
+
 def run_processing():
 
     file_path = '{0}/data/input/MST2.dat'.format(program_root)
@@ -70,8 +127,9 @@ def run_processing():
     if not graphs_data:
         return
 
-    for edges in graphs_data.values():
-        print edges
+    for graph_data in graphs_data.values():
+        # print graph_data['edges']
+        process_graph(graph_data)
 
 
 if __name__ == "__main__":
